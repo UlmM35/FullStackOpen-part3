747b1262589b24938af99524970c21c4d5988f48
ulm <125765792+UlmM35@users.noreply.github.com.>
2025-07-30 20:00:13 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -27,6 +28,7 @@ let persons = [
 
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 
 morgan.token('type', (req, res) => {
     return `${JSON.stringify(req.body)}`
